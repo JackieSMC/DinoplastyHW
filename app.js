6c31668ae8a39cb9ce2a59750635dd72eb8d2e8b
Jackie Amador <jamador01@saintmarys.edu>
2017-06-09 07:46:04 -0400

    Added editable list item, Made sure ID gets set when editing, Add
    ed autofocus and required, updated array when changed, local storage setup and sty
    
    le additions- need to fix a bug when moving down/up twice- a typeerror

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ const app = {
         this.dinos = []
         this.max = 0
         this.list = document.querySelector(selectors.listSelector)
-        document.querySelector(selectors.formSelector).addEventListener('submit', this.addDino.bind(this))
+        document.querySelector(selectors.formSelector).addEventListener('submit', this.addDinoFromForm.bind(this))
 
         this.load() 
     },
 
-    load() {
-        const dinoJSON = localStorage.getItem('dinos')
-        const dinoArray = JSON.parse(dinoJSON)
-        if (dinoArray) {
-            dinoArry.reverse().map(this.addDino.bind(this))
-        }
-    },
-
-
-    addDino(ev) {
+    addDino(dino) {
         const listItem = this.renderListItem(dino)
         this.list.insertBefore(listItem, this.list.firstElementChild)
         this.dinos.unshift(dino)
@@ -37,7 +28,13 @@ const app = {
         ev.target.reset()
     },
 
-
+    load() {
+        const dinoJSON = localStorage.getItem('dinos')
+        const dinoArray = JSON.parse(dinoJSON)
+        if (dinoArray) {
+            dinoArray.reverse().map(this.addDino.bind(this))
+        }
+    },
 
     save() {
         localStorage.setItem('dinos', JSON.stringify(this.dinos))
@@ -73,23 +70,34 @@ const app = {
         buttonDiv.appendChild(promote)
         buttonDiv.appendChild(deleteButton)
 
+        const span = document.createElement('span')
+        span.setAttribute('contentediable', '')
+        item.appendChild(span)
+        span.addEventListener('mouseout', this.refreshID.bind(this))
+        span.addEventListener('click', this.refreshID.bind(this))
+        span.addEventListener('mouseover', this.refreshID.bind(this))
+        
+        span.textContent = dino.name
+        item.setAttribute ('id', `${span.innerText}`)
+
         item.appendChild(buttonDiv)
 
         up.addEventListener('click', this.moveUp.bind(this))
+        down.addEventListener('click', this.moveDown.bind(this))
         promote.addEventListener('click', this.addFavorite.bind(this))
         deleteButton.addEventListener('click', this.deleteItem.bind(this))
 
         return item
     },
       addFavorite(ev) {
-      ev.preventDefault()
-      const listHTML = ev.target.parentElement.parentElement
+        ev.preventDefault()
+        const listHTML = ev.target.parentElement.parentElement
 
-      if(listHTML.style.backgroundColor == 'lightblue') {
-        listHTML.style.backgroundColor = 'whitesmoke'
-      } else {
-        listHTML.style.backgroundColor = 'lightblue'
-      }
+        if(listHTML.style.backgroundColor === 'lightblue') {
+            listHTML.style.backgroundColor = 'whitesmoke'
+        } else {
+            listHTML.style.backgroundColor = 'lightblue'
+        }
   },
   
   deleteItem(ev) {
@@ -97,26 +105,16 @@ const app = {
       const listHTML = ev.target.parentElement.parentElement
       
       for(let i = 0; i < this.dinos.length; i++) {
-        if(`i${this.dinos[i]}` == `${listHTML.getAttribute('id')}`) {
+        if(`${this.dinos[i].name}` === `${listHTML.getAttribute('id')}`) {
              this.dinos.splice(i, 1);
+             break
         }
       }
+      this.save()
       listHTML.remove()
   },
 
-  moveUp(ev) {
-    ev.preventDefault()
-    const listHTML = ev.target.parentElement.parentElement
-    //console.log(listHTML)
-
-    const upper = listHTML.previousSibling
-    //console.log(upper)
-    listHTML.innerText = upper.getAttribute('id')
-    upper.innerText = listHTML.getAttribute('id')
-
-    listHTML.setAttribute('id', `${upper.innerText}`)
-    upper.setAttribute('id', `${upper.innerText}`)
-
+  buttonGroup () {
     const buttonDiv = document.createElement('div')
     buttonDiv.setAttribute('class', 'button-group')
 
@@ -141,45 +139,117 @@ const app = {
     buttonDiv.appendChild(promote)
     buttonDiv.appendChild(deleteButton)
 
-
-    listHTML.appendChild(buttonDiv)
-
     up.addEventListener('click', this.moveUp.bind(this))
+    down.addEventListener('click', this.moveDown.bind(this))
     promote.addEventListener('click', this.addFavorite.bind(this))
     deleteButton.addEventListener('click', this.deleteItem.bind(this))
 
-
-    const buttonDiv2 = document.createElement('div')
-    buttonDiv2.setAttribute('class', 'button-group')
-
-    const up2 = document.createElement('a')
-    up2.setAttribute('class', 'secondary button')
-    up2.textContent = 'Up'
-
-    const down2 = document.createElement('a')
-    down2.setAttribute('class', 'primary button')
-    down2.textContent = 'Down'
-
-    const promote2 = document.createElement('a')
-    promote2.setAttribute('class', 'success button')
-    promote2.textContent = 'Promote'
-
-    const deleteButton2 = document.createElement('a')
-    deleteButton2.setAttribute('class', 'alert button')
-    deleteButton2.textContent = 'Delete'
-
-    buttonDiv2.appendChild(up2)
-    buttonDiv2.appendChild(down2)
-    buttonDiv2.appendChild(promote2)
-    buttonDiv2.appendChild(deleteButton2)
-
-    upper.appendChild(buttonDiv2)
-
-    up2.addEventListener('click', this.moveUp.bind(this))
-    promote2.addEventListener('click', this.addFavorite.bind(this))
-    deleteButton2.addEventListener('click', this.deleteItem.bind(this))
-
+    return buttonDiv
   },
+
+  moveUp(ev) {
+    ev.preventDefault()
+    const listHTML = ev.target.parentElement.parentElement
+    const upper = listHTML.previousSibling
+    
+    listHTML.innerText = upper.getAttribute('id')
+    upper.innerText = listHTML.getAttribute('id')
+    const listHTMLColor = listHTML.style.backgroundColor
+    const upperHTMLColor = upper.style.backgroundColor
+
+    const next = upper.nextSibling
+
+    listHTML.setAttribute('id', `${upper.innerText}`)
+    upper.setAttribute('id', `${upper.innerText}`)
+    next.setAttribute('id', `${next.innerText}`)
+
+    const firstButtonGroup = this.buttonGroup()
+    listHTML.appendChild(firstButtonGroup)
+
+    const secondButtonGroup = this.buttonGroup()
+    upper.appendChild(secondButtonGroup)
+
+    if(listHTMLColor === 'lightblue' && upperHTMLColor === 'lightblue') {
+        next.style.backgroundColor = 'lightblue'
+        upper.style.backgroundColor = 'lightblue'
+    } else if (listHTMLColor === 'lightblue') {
+        upper.style.backgroundColor = 'lightblue'
+        next.style.backgroundColor = 'magenta'
+    } else if (upperHTMLColor === 'lightblue') {
+        upper.style.backgroundColor = 'magenta'
+        next.style.backgroundColor = 'lightblue'
+    } else {
+        upper.style.backgroundColor = 'magenta'
+        next.style.backgroundColor = 'magenta'
+    }
+    const newNext = next.previousSibling
+    for(let i = 0; i < this.dinos.length; i++) {
+      if (this.dinos[i].name === newNext.getAttribute('id')) {
+        this.dinos[i].name = `${listHTML.getAttribute('id')}`
+        this.dinos[i-1].name = `${newNext.getAttribute('id')}`
+      }
+    }
+    this.save()
+},
+    moveDown(ev) {
+        ev.preventDefault()
+        const listHTML = ev.target.parentElement.parentElement
+        const lower = listHTML.nextSibling
+
+        listHTML.innerText = lower.getAttribute('id')
+        lower.innerText = listHTML.getAttribute('id')
+        const listHTMLColor = listHTML.style.backgroundColor
+        const lowerHTMLColor = lower.style.backgroundColor
+
+        const upper = lower.previousSibling
+
+        listHTML.setAttribute('id', `${lower.innerText}`)
+        lower.setAttribute('id', `${upper.innerText}`)
+        upper.setAttribute('id', `${upper.innerText}`)
+
+        const firstButtonGroup = this.buttonGroup()
+        listHTML.appendChild(firstButtonGroup)
+
+        const secondButtonGroup = this.buttonGroup()
+        lower.appendChild(secondButtonGroup)
+
+    if(listHTMLColor === 'lightblue' && lowerHTMLColor === 'lightblue') {
+        upper.style.backgroundColor = 'lightblue'
+        lower.style.backgroundColor = 'lightblue'
+    } else if (listHTMLColor === 'lightblue') {
+        lower.style.backgroundColor = 'lightblue'
+        upper.style.backgroundColor = 'magenta'
+    } else if (lowerHTMLColor === 'lightblue') {
+        lower.style.backgroundColor = 'magenta'
+        upper.style.backgroundColor = 'lightblue'
+    } else {
+        lower.style.backgroundColor = 'magenta'
+        upper.style.backgroundColor = 'magenta'
+    }
+
+    for(let i = 0; i < this.dinos.length; i++) {
+      if (this.dinos[i].name === listHTML.getAttribute('id')) {
+        this.dinos[i].name = `${lower.getAttribute('id')}`
+        this.dinos[i-1].name = `${listHTML.getAttribute('id')}`
+      }
+    }
+    this.save()
+
+},
+
+   refreshID(ev) {
+       ev.preventDefault()
+       const listHTML = ev.target.parentElement
+       const lower = listHTML.childNodes[0]
+
+       for(let i=0; i < this.dinos.length; i++) {
+           if (this.dinos[i].name == listHTML.getAttribute('id')) {
+            this.dinos[i].name = lower.innerText
+           }
+       }
+       listHTML.setAttribute('id', `${lower.innerText}`)
+       this.save()
+   } 
 
 }
 
